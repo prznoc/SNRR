147633bc430d0e972f5bfd77ff602368684b4f83
prznoc <przemyslaw.nocon19@gmail.com>
2020-06-09 11:00:03 +0200

    Vertical RecycleView

--- app/src/main/java/com/example/snrr/MyListAdapter.java
+++ app/src/main/java/com/example/snrr/MyListAdapter.java
@@ -3,15 +3,18 @@ package com.example.snrr;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.myViewHolder>{
     private Product[] listdata;
 
     // RecyclerView recyclerView;
@@ -19,19 +22,19 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         this.listdata = listdata;
     }
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public myViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(listItem);
+        View listItem = layoutInflater.inflate(R.layout.list_content, parent, false);
+        myViewHolder viewHolder = new myViewHolder(listItem);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(myViewHolder holder, int position) {
         final Product myListData = listdata[position];
         holder.textView.setText(listdata[position].getName());
         holder.imageView.setImageResource (listdata[position].getImage());
-        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+        holder.cardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(),"click on item: "+myListData.getDescription(),Toast.LENGTH_LONG).show();
@@ -44,15 +47,15 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         return listdata.length;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class myViewHolder extends RecyclerView.ViewHolder {
         public ImageView imageView;
         public TextView textView;
-        public RelativeLayout relativeLayout;
-        public ViewHolder(View itemView) {
+        public FrameLayout cardview;
+        public myViewHolder(View itemView) {
             super(itemView);
             this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
             this.textView = (TextView) itemView.findViewById(R.id.textView);
-            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.relativeLayout);
+            cardview = (FrameLayout)itemView.findViewById(R.id.linearLayout);
         }
     }
 }
