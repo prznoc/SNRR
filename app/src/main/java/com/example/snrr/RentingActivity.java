f46244b2c11bbdc43cad9830380313d016e4f743
prznoc <przemyslaw.nocon19@gmail.com>
2020-06-21 16:54:57 +0200

    Broadcast fixes 1/2

--- app/src/main/java/com/example/snrr/RentingActivity.java
+++ app/src/main/java/com/example/snrr/RentingActivity.java
@@ -8,6 +8,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,19 +30,31 @@ public class RentingActivity extends AppCompatActivity {
     public void checkOutOnClick(View view){
         CheckBox checkBox = findViewById(R.id.checkBox);
         if(checkBox.isChecked()) {
-            String name = findViewById(R.id.editTextTextPersonName).toString();
+            Boolean proper_data = true;
+            EditText text = (EditText) findViewById(R.id.editTextTextPersonName);
+            String name = text.getText().toString();
+            if (name.isEmpty()) proper_data = false;
             String email = findViewById(R.id.editTextTextEmailAddress).toString();
             String phone = findViewById(R.id.editTextPhone).toString();
             String postalAddress = findViewById(R.id.editTextTextPostalAddress).toString();
             String dateFrom = findViewById(R.id.editTextDate).toString();
             String dateTo = findViewById(R.id.editTextDate2).toString();
-            LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
-            Intent localIntent = new Intent("New Order")
-                    .putExtra("Data",name + email + phone + postalAddress + dateFrom + dateTo);
-            localBroadcastManager.sendBroadcast(localIntent);
-            final Intent send_intent = new Intent(this, ConfirmActivity.class);
-            send_intent.putExtra("product", product);
-            startActivity(send_intent);
+            if(proper_data) {
+                LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
+                Intent localIntent = new Intent("New Order")
+                        .putExtra("name", name)
+                        .putExtra("email", email)
+                        .putExtra("phone", phone)
+                        .putExtra("postalAddress", postalAddress)
+                        .putExtra("dateFrom", dateFrom)
+                        .putExtra("dateTo", dateTo)
+                        .putExtra("product_name", product.getName());
+                localBroadcastManager.sendBroadcast(localIntent);
+                final Intent send_intent = new Intent(this, ConfirmActivity.class);
+                send_intent.putExtra("product", product);
+                startActivity(send_intent);
+            }
+            else Toast.makeText(getApplicationContext(),"Please enter proper data",Toast.LENGTH_LONG).show();
         }
         else{
             Toast.makeText(getApplicationContext(),"Please accept our policy",Toast.LENGTH_LONG).show();
